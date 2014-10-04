80df2164a2ea1e75903d1b209f74b35f5b5444e0
Michael Müller <michael@fds-team.de>
2014-10-04 02:53:22 +0200

    ntdll: Setup a temporary signal handler during process startup to handle page faults. (v2)

--- dlls/ntdll/unix/signal_x86_64.c
+++ dlls/ntdll/unix/signal_x86_64.c
@@ -2908,6 +2908,12 @@ void *signal_init_syscalls(void)
     return syscall_dispatcher;
 }
 
+/**********************************************************************
+ *    signal_init_early
+ */
+void signal_init_early(void)
+{
+}
 
 /***********************************************************************
  *           init_thread_context
