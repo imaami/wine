091fb0f99cf1ab573e949389d0dc21159d505384
Sebastian Lackner <sebastian@fds-team.de>
2014-11-20 13:27:24 +0100

    ws2_32: Implement returning the proper time with SO_CONNECT_TIME.
    Based on a patch by Erich Hoover.
    
    To avoid calculations on the client-side, the server sends the connect time as relative (negative) value.
    Needs tools/make_requests.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -1777,6 +1777,7 @@ struct get_socket_info_reply
     int type;
     int protocol;
     char __pad_20[4];
+    timeout_t connect_time;
 };
 
 
@@ -6280,7 +6281,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 659
+#define SERVER_PROTOCOL_VERSION 660
 
 /* ### protocol_version end ### */
 
