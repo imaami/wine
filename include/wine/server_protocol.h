5dc8f00b5472bd8a1155e2d573bfc1fbac3dee2d
Dmitry Timoshkov <dmitry@baikal.ru>
2017-05-17 23:55:55 +0800

    server: Add support for a layered window region. (v3)

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -3396,6 +3396,19 @@ struct set_window_region_reply
 
 
 
+struct set_layer_region_request
+{
+    struct request_header __header;
+    user_handle_t  window;
+    /* VARARG(region,rectangles); */
+};
+struct set_layer_region_reply
+{
+    struct reply_header __header;
+};
+
+
+
 struct get_update_region_request
 {
     struct request_header __header;
@@ -5645,6 +5658,7 @@ enum request
     REQ_get_surface_region,
     REQ_get_window_region,
     REQ_set_window_region,
+    REQ_set_layer_region,
     REQ_get_update_region,
     REQ_update_window_zorder,
     REQ_redraw_window,
@@ -5932,6 +5946,7 @@ union generic_request
     struct get_surface_region_request get_surface_region_request;
     struct get_window_region_request get_window_region_request;
     struct set_window_region_request set_window_region_request;
+    struct set_layer_region_request set_layer_region_request;
     struct get_update_region_request get_update_region_request;
     struct update_window_zorder_request update_window_zorder_request;
     struct redraw_window_request redraw_window_request;
@@ -6217,6 +6232,7 @@ union generic_reply
     struct get_surface_region_reply get_surface_region_reply;
     struct get_window_region_reply get_window_region_reply;
     struct set_window_region_reply set_window_region_reply;
+    struct set_layer_region_reply set_layer_region_reply;
     struct get_update_region_reply get_update_region_reply;
     struct update_window_zorder_reply update_window_zorder_reply;
     struct redraw_window_reply redraw_window_reply;
@@ -6350,7 +6366,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 705
+#define SERVER_PROTOCOL_VERSION 706
 
 /* ### protocol_version end ### */
 
