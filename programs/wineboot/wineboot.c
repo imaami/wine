ca9cfd5111781a2d4922ccd856a97a0327685497
Andrew Eikum <aeikum@codeweavers.com>
2018-01-22 14:35:51 -0600

    HACK: wineboot: Don't show "updating prefix" window

--- programs/wineboot/wineboot.c
+++ programs/wineboot/wineboot.c
@@ -1059,6 +1059,7 @@ static INT_PTR CALLBACK wait_dlgproc( HWND hwnd, UINT msg, WPARAM wp, LPARAM lp 
     return 0;
 }
 
+/*
 static HWND show_wait_window(void)
 {
     const char *config_dir = prettyprint_configdir();
@@ -1075,6 +1076,7 @@ static HWND show_wait_window(void)
     HeapFree( GetProcessHeap(), 0, name );
     return hwnd;
 }
+*/
 
 static HANDLE start_rundll32( const WCHAR *inf_path, BOOL wow64 )
 {
@@ -1150,7 +1152,7 @@ static void update_wineprefix( BOOL force )
 
         if ((process = start_rundll32( inf_path, FALSE )))
         {
-            HWND hwnd = show_wait_window();
+/*            HWND hwnd = show_wait_window();*/
             for (;;)
             {
                 MSG msg;
@@ -1162,7 +1164,7 @@ static void update_wineprefix( BOOL force )
                 }
                 else while (PeekMessageW( &msg, 0, 0, 0, PM_REMOVE )) DispatchMessageW( &msg );
             }
-            DestroyWindow( hwnd );
+/*            DestroyWindow( hwnd );*/
         }
         WINE_MESSAGE( "wine: configuration in '%s' has been updated.\n", prettyprint_configdir() );
     }
