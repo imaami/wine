c8e7ffa28ddfe7204a8d79b12e28a387088176c3
Zebediah Figura <z.figura12@gmail.com>
2018-06-07 20:09:59 -0500

    server: Create server objects for eventfd-based synchronization objects.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5784,6 +5784,22 @@ struct resume_process_reply
 };
 
 
+struct create_esync_request
+{
+    struct request_header __header;
+    unsigned int access;
+    int          initval;
+    int          flags;
+    /* VARARG(objattr,object_attributes); */
+};
+struct create_esync_reply
+{
+    struct reply_header __header;
+    obj_handle_t handle;
+    char __pad_12[4];
+};
+
+
 enum request
 {
     REQ_new_process,
@@ -6083,6 +6099,7 @@ enum request
     REQ_terminate_job,
     REQ_suspend_process,
     REQ_resume_process,
+    REQ_create_esync,
     REQ_NB_REQUESTS
 };
 
@@ -6387,6 +6404,7 @@ union generic_request
     struct terminate_job_request terminate_job_request;
     struct suspend_process_request suspend_process_request;
     struct resume_process_request resume_process_request;
+    struct create_esync_request create_esync_request;
 };
 union generic_reply
 {
@@ -6689,8 +6707,9 @@ union generic_reply
     struct terminate_job_reply terminate_job_reply;
     struct suspend_process_reply suspend_process_reply;
     struct resume_process_reply resume_process_reply;
+    struct create_esync_reply create_esync_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 588
+#define SERVER_PROTOCOL_VERSION 589
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
