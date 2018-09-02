8ccccdbdc977b8d0d21bb6ec2d04914170b32b0e
Zebediah Figura <z.figura12@gmail.com>
2018-09-01 19:24:15 -0500

    server, ntdll: Implement message waits.
    The approach of giving the queue fd to ntdll to wait on doesn't work here.
    Fortunately, the way esync has ended up lends itself very easily to a rather
    clean approach: let the server do it the normal way.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5907,6 +5907,16 @@ struct get_fsync_idx_reply
     unsigned int shm_idx;
 };
 
+struct fsync_msgwait_request
+{
+    struct request_header __header;
+    int          in_msgwait;
+};
+struct fsync_msgwait_reply
+{
+    struct reply_header __header;
+};
+
 
 enum request
 {
@@ -6214,6 +6224,7 @@ enum request
     REQ_esync_msgwait,
     REQ_create_fsync,
     REQ_get_fsync_idx,
+    REQ_fsync_msgwait,
     REQ_NB_REQUESTS
 };
 
@@ -6525,6 +6536,7 @@ union generic_request
     struct esync_msgwait_request esync_msgwait_request;
     struct create_fsync_request create_fsync_request;
     struct get_fsync_idx_request get_fsync_idx_request;
+    struct fsync_msgwait_request fsync_msgwait_request;
 };
 union generic_reply
 {
@@ -6834,8 +6846,9 @@ union generic_reply
     struct esync_msgwait_reply esync_msgwait_reply;
     struct create_fsync_reply create_fsync_reply;
     struct get_fsync_idx_reply get_fsync_idx_reply;
+    struct fsync_msgwait_reply fsync_msgwait_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 604
+#define SERVER_PROTOCOL_VERSION 605
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
