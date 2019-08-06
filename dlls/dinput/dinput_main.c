9ddbd1ede19fc81b62178add44ccf01da7b786f6
Andrew Eikum <aeikum@codeweavers.com>
2019-08-06 08:42:14 -0500

    dinput: Only enumerate SDL devices

--- dlls/dinput/dinput_main.c
+++ dlls/dinput/dinput_main.c
@@ -92,9 +92,6 @@ static const struct dinput_device *dinput_devices[] =
     &mouse_device,
     &keyboard_device,
     &joystick_sdl_device,
-    &joystick_linuxinput_device,
-    &joystick_linux_device,
-    &joystick_osx_device
 };
 
 HINSTANCE DINPUT_instance;
