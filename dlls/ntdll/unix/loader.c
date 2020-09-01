b7fa729885481d11a5de180491f00b6e53abb167
Zebediah Figura <z.figura12@gmail.com>
2020-08-31 23:55:29 -0500

    ntdll: Get rid of the direct futex path for condition variables.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1820,8 +1820,6 @@ static struct unix_funcs unix_funcs =
     fast_RtlAcquireSRWLockShared,
     fast_RtlReleaseSRWLockExclusive,
     fast_RtlReleaseSRWLockShared,
-    fast_RtlWakeConditionVariable,
-    fast_wait_cv,
     ntdll_atan,
     ntdll_ceil,
     ntdll_cos,
--- dlls/ntdll/unixlib.h
+++ dlls/ntdll/unixlib.h
@@ -26,7 +26,7 @@
 struct _DISPATCHER_CONTEXT;
 
 /* increment this when you change the function table */
-#define NTDLL_UNIXLIB_VERSION 122
+#define NTDLL_UNIXLIB_VERSION 123
 
 struct unix_funcs
 {
@@ -46,9 +46,6 @@ struct unix_funcs
     NTSTATUS      (CDECL *fast_RtlAcquireSRWLockShared)( RTL_SRWLOCK *lock );
     NTSTATUS      (CDECL *fast_RtlReleaseSRWLockExclusive)( RTL_SRWLOCK *lock );
     NTSTATUS      (CDECL *fast_RtlReleaseSRWLockShared)( RTL_SRWLOCK *lock );
-    NTSTATUS      (CDECL *fast_RtlWakeConditionVariable)( RTL_CONDITION_VARIABLE *variable, int count );
-    NTSTATUS      (CDECL *fast_wait_cv)( RTL_CONDITION_VARIABLE *variable, const void *value,
-                                         const LARGE_INTEGER *timeout );
 
     /* math functions */
     double        (CDECL *atan)( double d );
