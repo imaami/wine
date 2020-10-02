ed2a2f126daff018238113b6adba814453787f89
Derek Lesho <dlesho@codeweavers.com>
2020-10-02 12:11:49 -0500

    bcrypt: Implement BCryptSecretAgreement with libgcrypt.
    Signed-off-by: Derek Lesho <dlesho@codeweavers.com>

--- dlls/bcrypt/macos.c
+++ dlls/bcrypt/macos.c
@@ -284,7 +284,8 @@ static const struct key_funcs key_funcs =
     key_export_dsa_capi,
     key_export_ecc,
     key_import_dsa_capi,
-    key_import_ecc
+    key_import_ecc,
+    NULL
 };
 
 struct key_funcs * macos_lib_init( DWORD reason )
