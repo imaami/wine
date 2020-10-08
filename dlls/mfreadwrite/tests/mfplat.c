3644c5369747b5d7a970da8093b0aa1713083377
Derek Lesho <dlesho@codeweavers.com>
2020-10-08 16:39:53 -0500

    winegstreamer: Implement IMFMediaStream::RequestSample.
    Signed-off-by: Derek Lesho <dlesho@codeweavers.com>

--- dlls/mfreadwrite/tests/mfplat.c
+++ dlls/mfreadwrite/tests/mfplat.c
@@ -727,7 +727,6 @@ static void test_source_reader(void)
 
     hr = IMFSourceReader_ReadSample(reader, MF_SOURCE_READER_FIRST_AUDIO_STREAM, 0, &actual_index, &stream_flags,
             &timestamp, &sample);
-todo_wine
     ok(hr == S_OK, "Failed to get a sample, hr %#x.\n", hr);
     if (hr != S_OK)
         goto skip_read_sample;
@@ -753,8 +752,11 @@ todo_wine
             &timestamp, &sample);
     ok(hr == S_OK, "Failed to get a sample, hr %#x.\n", hr);
     ok(actual_index == 0, "Unexpected stream index %u\n", actual_index);
+todo_wine
+{
     ok(stream_flags == MF_SOURCE_READERF_ENDOFSTREAM, "Unexpected stream flags %#x.\n", stream_flags);
     ok(!sample, "Unexpected sample object.\n");
+}
 
     hr = IMFSourceReader_ReadSample(reader, MF_SOURCE_READER_FIRST_AUDIO_STREAM, MF_SOURCE_READER_CONTROLF_DRAIN,
             &actual_index, &stream_flags, &timestamp, &sample);
