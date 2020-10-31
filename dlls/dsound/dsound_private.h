wine-compholio/wine-patched

dsound: Apply filters before sound is multiplied to speakers.
Based on a patch by Mark Harmstone.

dlls/dsound/dsound_private.h
@@ -91,8 +91,8 @@ struct DirectSoundDevice
     int                         speaker_num[DS_MAX_CHANNELS];
     int                         num_speakers;
     int                         lfe_channel;
-    float *mix_buffer, *tmp_buffer;
-    DWORD                       tmp_buffer_len, mix_buffer_len;
+    float                      *mix_buffer, *tmp_buffer, *dsp_buffer;
+    DWORD                       mix_buffer_len, tmp_buffer_len, dsp_buffer_len;
 
     DSVOLUMEPAN                 volpan;
 

imaami/wine

dlls/dsound/dsound_private.h
@@ -89,8 +89,8 @@ struct DirectSoundDevice
     int                         speaker_num[DS_MAX_CHANNELS];
     int                         num_speakers;
     int                         lfe_channel;
-    float *tmp_buffer, *cp_buffer;
-    DWORD                       tmp_buffer_len, cp_buffer_len;
+    float *tmp_buffer, *cp_buffer, *dsp_buffer;
+    DWORD                       tmp_buffer_len, cp_buffer_len, dsp_buffer_len;
 
     DSVOLUMEPAN                 volpan;
 