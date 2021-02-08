8e33edcd084a0f0f34cb0131026d5b8417d00dc6
Zebediah Figura <z.figura12@gmail.com>
2021-02-07 22:54:19 -0600

    server: Create processes using a limited administrator token by default.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>

--- server/process.c
+++ server/process.c
@@ -585,7 +585,7 @@ struct process *create_process( int fd, struct process *parent, unsigned int fla
     if (!parent)
     {
         process->handles = alloc_handle_table( process, 0 );
-        process->token = token_create_admin( TokenElevationTypeFull );
+        process->token = token_create_admin( TokenElevationTypeLimited );
         process->affinity = ~0;
     }
     else
